1a6f5580387be3f506642d785cba088ef9dcc003
sergio <s_mart53@list.ru>
2025-03-01 23:20:27 +0300

    Update setup.py

--- hanoi/hanoi_cli/setup.py
+++ hanoi/hanoi_cli/setup.py
@@ -1,1 +1,11 @@
+import setuptools as st
 
+st.setup(
+    name='hanoi_cli',
+    version='1.0.0',
+    py_modules=['hanoi_cli'],
+    entry_points={
+       'console_scripts':
+           'hanoi_cli=hanoi_cli:main',
+    },
+)
