e81d76b7d92e10f13353f6a0b4f87ecee4d75b1e
sergio <s_mart53@list.ru>
2026-05-28 15:43:27 +0300

    Auto-update: EUCLIDE/euclide.1.py

--- EUCLIDE/euclide.1.py
+++ EUCLIDE/euclide.1.py
@@ -1,3 +1,54 @@
+"""
+  https://www.euclidea.xyz/
+  https://geocentral.net/geometria/
+  https://www.geometryexpressions.com/gxweb/
+  https://sketchpad.keycurriculum.com/
+  https://www.mathspad.co.uk/index.php
+  https://www.omnigeometry.com/sacred-geometry-software/
+  https://www.mozaweb.com/en/euklides
+  https://github.com/zdule/Geometry
+
+  https://en.wikipedia.org/wiki/List_of_interactive_geometry_software
+  https://geoscript.app/
+  https://www.robocompass.com/app
+
+  https://www.math.net/geometric-construction
+  https://www.academia.edu/31600279/Geometric_Constructions
+  https://thirdspacelearning.com/gcse-maths/geometry-and-measure/constructions/
+  https://livephysics.com/tools/geometry/
+  https://www.gogeometry.com/
+
+  https://ellis2020.org/iTLG/Student%20Reference%20Book/Geometry%20and%20Constructrions.pdf
+  http://www.epab.bme.hu/oktatas/2013-2014-2/e-GeoC1/GC1_Lecture_notes.pdf
+  https://www.geogebra.org/m/Xfayrrj8
+  https://www.geogebra.org/m/s8vbbmjd
+  https://www.mathsisfun.com/geometry/constructions.html
+  https://www.storyofmathematics.com/geometric-construction/
+  https://mathematix.net/wp-content/uploads/2024/03/11.Geomtrical-Constructions.pdf
+  https://www.mathopenref.com/constructions.html
+  https://www.whistleralley.com/construction/reference.htm
+  https://mathsux.org/2022/03/30/geometry-constructions/
+  https://engineeringtechnology.org/engineering-graphics/applied-and-descriptive-geometry/geometric-construction/
+
+  https://tutors.com/lesson/geometric-constructions
+  https://mathbitsnotebook.com/Geometry/Constructions/CCinfo.html
+  https://www.onlinemathlearning.com/geometry-construction.html
+  https://www.cuemath.com/geometry/geometric-construction/
+
+  https://cs.stanford.edu/~aozdemir/blog/construct-mohr-mascheroni/
+  https://github.com/alex-ozdemir/construct/wiki/Introduction-to-Construct
+  https://www.cs.hmc.edu/~aozdemir/construct/
+
+  https://poincare.matf.bg.ac.rs/~janicic/gclc/
+  https://poincare.matf.bg.ac.rs/~janicic/gclc/gclc_man.pdf
+  https://poincare.matf.bg.ac.rs/~predrag.janicic/gclc/gclc-web/
+
+  https://github.com/geometor
+  https://geometor.github.io/model/
+  https://github.com/geometor/model
+  https://github.com/geometor/explorer
+"""
+
 import math
 from ipycanvas import Canvas, MultiCanvas, hold_canvas
 import ipywidgets as widgets
